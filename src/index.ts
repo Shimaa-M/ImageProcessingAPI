d155be68c1fbe59b1b84217b29c9ebb487e999fa
Shimo Mahmoud <eng.shimaamahmoud@gmail.com>
2022-02-16 11:49:49 +0200

    add npm chache

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,16 @@
 import express from 'express';
-import resize from './utilities/resize-image';
-import { promises as fsPromises } from 'fs';
-import url from 'url';
+import imageRoute from './routes/imageRoute';
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(express.static('assets'));
-app.use('/images', express.static('images'));
+app.use('/image', express.static('images'));
 
-app.get('/images', async (req, res) => {
-  const params = await JSON.parse(
-    '{"' +
-      decodeURI(url.parse(req.url).query as unknown as string)
-        .replace(/&/g, '","')
-        .replace(/=/g, '":"') +
-      '"}'
-  );
-  const filename: string = params.filename;
-  const width: number = parseInt(params.width);
-  const height: number = parseInt(params.height);
-  await resize(filename, width, height);
-  res.writeHead(301, { 'Content-Type': 'image/jpg' });
-  res.end(await fsPromises.readFile(`./assets/thumb/${filename}.jpg`));
-});
+app.use('/image' , imageRoute);
+
 
 app.listen(port, () => {
-  console.log('server is running on port 5000');
+  console.log('server is running on port 3000');
 });
 export default app;
