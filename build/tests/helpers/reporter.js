a42075992e65230c4e4d67ee9654229e90db8358
Shimo Mahmoud <eng.shimaamahmoud@gmail.com>
2022-02-16 11:52:14 +0200

    modified

--- build/tests/helpers/reporter.js
+++ build/tests/helpers/reporter.js
@@ -1,16 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const jasmine_spec_reporter_1 = require("jasmine-spec-reporter");
-// class CustomProcessor extends DisplayProcessor {
-//     public displayJasmineStarted(info: SuiteInfo, log: unknown): unknown {
-//         return `${log}`;
-//     }
-// }
 jasmine.getEnv().clearReporters();
-// jasmine.getEnv().addReporter(new SpecReporter({
-//     spec: {
-//         displayStacktrace: StacktraceOption.NONE
-//     },
-//     customProcessors: [CustomProcessor],
-// }));
 jasmine.getEnv().addReporter(new jasmine_spec_reporter_1.SpecReporter());
